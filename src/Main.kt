a83b9d5aa25d1fdd33bdb93fbb9101104f6a55bc
emkat93 <eallen5064@students.mchenry.edu>
2020-08-31 18:15:28 -0500

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,31 @@
+fun main(){
+// arithmetic operators (+,-,*,/,%)
+    var a = 15
+    var b = 60
+    var c = 100
+    var d = 35
+
+    var ans1 = a * b
+    println(ans1)
+
+    var ans2 = (d-b) + (a/c)
+    println(ans2)
+
+
+// comparison operators (==, !=, <, >, <=, >=)
+    var isGreaterThan = d > c
+        println(isGreaterThan)
+
+
+// assignment operators (+=,-=,*=,/=,%=)
+    var ourAnswer = 0
+    ourAnswer -= 10
+    println(ourAnswer)
+
+
+// increment & decrement operators (++,--)
+    ourAnswer--
+    println(ourAnswer)
+
+
+}
